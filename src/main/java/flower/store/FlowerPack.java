e020fd00d69f957bc96990e50af21828e220310e
krasniukevych-marichka <m.krasniukevych@gmail.com>
2025-10-08 17:51:13 +0300

    Implemented FlowerPack class

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,18 @@
 package flower.store;
 
+import lombok.Data;
+
+@Data
 public class FlowerPack {
     private Flower flower;
-    private int quantity;
+    private int amount;
+
+    public FlowerPack(Flower flower, int amount) {
+        this.flower = new Flower(flower);
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return flower.getPrice()*amount;
+    }
 }
