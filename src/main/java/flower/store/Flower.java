f6a11f2e5b5ac8c96f9fa7dd984d3ed573504570
krasniukevych-marichka <m.krasniukevych@gmail.com>
2025-10-08 17:50:51 +0300

    Fixed Flower class

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,21 +1,23 @@
 package flower.store;
 
-import lombok.Getter;
-import lombok.Setter;
-
-
-
-@Setter
+import lombok.NoArgsConstructor;
+import lombok.Data;
+@Data
+@NoArgsConstructor
 public class Flower {
-    @Getter
     private double sepalLength;
     private FlowerColor color;
-    @Getter
     private double price;
-    @Getter
     private FlowerType flowerType;
 
     public String getColor() {
         return color.toString();
+    }
+
+    public Flower(Flower other) {
+        this.color = other.color;
+        this.flowerType = other.flowerType;
+        this.sepalLength = other.sepalLength;
+        this.price = other.price;
     }
 }
